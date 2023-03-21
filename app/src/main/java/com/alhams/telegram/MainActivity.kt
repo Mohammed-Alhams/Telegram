ea52d35b8dac9e583cb6e6c4e3c477547f631861
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-03-21 16:28:04 +0200

    happy initial and almost last commit

---
+++ app/src/main/java/com/alhams/telegram/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alhams.telegram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.IllegalStateException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
